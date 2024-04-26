8a3b1f91f06b7685e05e4d23b760868ac3e63358
J.S.LI <jsli@linux.com>
2024-04-27 00:10:47 +0800

    Add home row mod as an layer.

--- keyboards/ducky/one2mini/keymaps/custom/keymap.c
+++ keyboards/ducky/one2mini/keymaps/custom/keymap.c
@@ -18,37 +18,39 @@
 // LAYERS
 enum Layer {
     _QWERTY=0,        // Standard QWERTY layer
+    _REGULAR,          // Regular key layer
     _FUNCTION,          // Function key layer
     _COLOUR             // RGB key layer
 };
 #define _QW _QWERTY
+#define _RG _REGULAR
 #define _FN _FUNCTION
 #define _CLR _COLOUR
 
 // Home row mod keys
 // Left-hand home row mods
-// #define HM_A LGUI_T(KC_A)
-// #define HM_S LALT_T(KC_S)
-// #define HM_D LCTL_T(KC_D)
-// #define HM_F LSFT_T(KC_F)
+#define HM_A LGUI_T(KC_A)
+#define HM_S LALT_T(KC_S)
+#define HM_D LCTL_T(KC_D)
+#define HM_F LSFT_T(KC_F)
 
 // Right-hand home row mods
-// #define HM_J RSFT_T(KC_J)
-// #define HM_K RCTL_T(KC_K)
-// #define HM_L LALT_T(KC_L)
-// #define HM_SCLN RGUI_T(KC_SCLN)
+#define HM_J RSFT_T(KC_J)
+#define HM_K RCTL_T(KC_K)
+#define HM_L LALT_T(KC_L)
+#define HM_SCLN RGUI_T(KC_SCLN)
 
-// Left-hand home row mods
-#define HM_A KC_A
-#define HM_S KC_S
-#define HM_D KC_D
-#define HM_F KC_F
-
-// Right-hand home row mods
-#define HM_J KC_J
-#define HM_K KC_K
-#define HM_L KC_L
-#define HM_SCLN KC_SCLN
+// // Left-hand home row mods
+// #define HM_A KC_A
+// #define HM_S KC_S
+// #define HM_D KC_D
+// #define HM_F KC_F
+//
+// // Right-hand home row mods
+// #define HM_J KC_J
+// #define HM_K KC_K
+// #define HM_L KC_L
+// #define HM_SCLN KC_SCLN
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -58,6 +60,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     MO(_FN), HM_A,    HM_S,    HM_D,    HM_F,    KC_G,    KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
+    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(_FN), KC_RALT, DF(_RG), KC_RCTL
+  ),
+
+  [_REGULAR] = LAYOUT_all(
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14
+    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(_FN), KC_RALT, DF(_QW), KC_RCTL
   ),
