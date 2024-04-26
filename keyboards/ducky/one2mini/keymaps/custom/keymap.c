bd6a7e6bae8fa6d0d4e4242703d417176a4d0089
JHIH-SIOU LI <jsli@linux.com>
2024-04-26 19:42:19 +0800

    Update keymap for simplicity and home row mod.

--- keyboards/ducky/one2mini/keymaps/custom/keymap.c
+++ keyboards/ducky/one2mini/keymaps/custom/keymap.c
@@ -17,33 +17,23 @@
 
 // LAYERS
 enum Layer {
-    _DVORAK = 0,
-    _QWERTY,        // Standard QWERTY layer
+    _QWERTY=0,        // Standard QWERTY layer
     _FUNCTION,          // Function key layer
     _COLOUR             // RGB key layer
 };
-#define _DV _DVORAK
 #define _QW _QWERTY
 #define _FN _FUNCTION
 #define _CLR _COLOUR
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_DVORAK] = LAYOUT_all(
-    //       2        3        4        5        6        7        8        9        10       11       12       13       14
-    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC,
-    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,
-    MO(_FN), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, KC_NUHS, KC_ENT,
-    KC_LSFT, KC_NUBS, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,             KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(_FN), KC_RALT, DF(_QW), KC_RCTL
-  ),
 
   [_QWERTY] = LAYOUT_all(
     //       2        3        4        5        6        7        8        9        10       11       12       13       14
     QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-    MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
+    MO(_FN), MT(MOD_LGUI, KC_A),    MT(MOD_LALT, KC_S),    MT(MOD_LCTL, KC_D),    MT(MOD_LSFT, KC_F),    KC_G,    KC_H,    MT(MOD_RSFT, KC_J),    MT(MOD_RCTL, KC_K),    MT(MOD_RALT, KC_L),    MT(MOD_RGUI, KC_SCLN), KC_QUOT, KC_NUHS, KC_ENT,
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(_FN), KC_RALT, DF(_DV), KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(_FN), KC_RALT, DF(_QW), KC_RCTL
   ),
 
   [_FUNCTION] = LAYOUT_all(
